97dd0745cdca9b2de7c6bd4d870b151065a704df
J <j@hph-2.local>
2022-01-07 16:48:11 +0000

    more pico tweaks

--- src/Companies.svelte
+++ src/Companies.svelte
@@ -1,30 +1,30 @@
 <h2>A few companies I've been involved in</h2>
 <div class="grid">
-    <div>
+    <div class="center">
         <img class="company" src="img/cheakamus.png" alt="Cheakamus" />
     </div>
-    <div>
+    <div class="center">
         <img class="company" src="img/strong.svg" alt="Strong Investments" />
     </div>
-    <div>
+    <div class="center">
         <img class="company" src="img/titan.svg" alt="Titan Research" />
     </div>
 </div>
 <div class="grid">
-    <div>
+    <div class="center">
         <img class="company" src="img/logo.svg" alt="Logo.com" />
     </div>
-    <div>
+    <div class="center">
         <img class="company" src="img/ocean.png" alt="Ocean Networks" />
     </div>
-    <div>
+    <div class="center">
         <img class="company" src="img/resume.png" alt="Resume.com" />
     </div>
 </div>
 <div class="grid">
-    <div />
-    <div>
+    <div class="center">
         <img class="company" src="img/namescon.png" alt="NamesCon" />
     </div>
+    <div />
     <div />
 </div>
