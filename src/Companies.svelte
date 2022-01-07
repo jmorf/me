6095ed6508275dae3ad762d4d04cd74a13b42376
J <j@hph.lan>
2022-01-07 21:44:25 +0000

    lazy loading

--- src/Companies.svelte
+++ src/Companies.svelte
@@ -2,7 +2,12 @@
     <h4>A few companies I've been involved in:</h4>
     <div class="grid">
         <div class="center">
-            <img class="company" src="img/cheakamus.png" alt="Cheakamus" />
+            <img
+                class="company"
+                src="img/cheakamus.png"
+                alt="Cheakamus"
+                loading="lazy"
+            />
         </div>
         <div class="center">
             <img
@@ -12,23 +17,48 @@
             />
         </div>
         <div class="center">
-            <img class="company" src="img/titan.svg" alt="Titan Research" />
+            <img
+                class="company"
+                src="img/titan.svg"
+                alt="Titan Research"
+                loading="lazy"
+            />
         </div>
     </div>
     <div class="grid">
         <div class="center">
-            <img class="company" src="img/logo.svg" alt="Logo.com" />
+            <img
+                class="company"
+                src="img/logo.svg"
+                alt="Logo.com"
+                loading="lazy"
+            />
         </div>
         <div class="center">
-            <img class="company" src="img/ocean.png" alt="Ocean Networks" />
+            <img
+                class="company"
+                src="img/ocean.png"
+                alt="Ocean Networks"
+                loading="lazy"
+            />
         </div>
         <div class="center">
-            <img class="company" src="img/resume.png" alt="Resume.com" />
+            <img
+                class="company"
+                src="img/resume.png"
+                alt="Resume.com"
+                loading="lazy"
+            />
         </div>
     </div>
     <div class="grid">
         <div class="center">
-            <img class="company" src="img/namescon.png" alt="NamesCon" />
+            <img
+                class="company"
+                src="img/namescon.png"
+                alt="NamesCon"
+                loading="lazy"
+            />
         </div>
         <div />
         <div />
