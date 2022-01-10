38f39470588ff6d7bb021e37c8caddf987a848a7
J <j@hph-2.local>
2022-01-10 12:10:30 +0000

    it seems app.css exists for a reason

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,43 +1,9 @@
 <script>
 	import Footer from "$lib/Footer.svelte";
+	import "../app.css";
 </script>
 
 <main class="container">
 	<slot />
 	<Footer />
 </main>
-
-<style>
-	body {
-		color: #fff;
-	}
-
-	.center {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
-	img.profile {
-		border: 4px solid #fff;
-		border-radius: 50%;
-		height: auto;
-		width: 18rem;
-	}
-
-	img.company {
-		color: #fff;
-		max-height: 3rem;
-		max-width: 50%;
-		height: auto;
-		width: auto;
-		margin: 2rem;
-		object-fit: contain;
-	}
-
-	svg.socials {
-		color: #fff;
-		height: 1.5rem;
-		margin-right: 1rem;
-	}
-</style>
